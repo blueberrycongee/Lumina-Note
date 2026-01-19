9bf313d9344c21d0b5521608c861128bfa3704a2
Andy wu <lpageo@163.com>
2026-01-20 01:47:26 +0800

    Add mixed-script shaping merge

--- src-tauri/src/typesetting/mod.rs
+++ src-tauri/src/typesetting/mod.rs
@@ -6,4 +6,4 @@ pub use font_manager::{
     FontData, FontError, FontManager, FontMapping, FontMetrics, ScriptKind,
 };
 pub use line_break::{break_glyph_run, BreakKind, LineBreak};
-pub use shaping::{shape_text, Glyph, GlyphRun, ShapingError};
+pub use shaping::{shape_mixed_text, shape_text, Glyph, GlyphRun, ShapingError};
