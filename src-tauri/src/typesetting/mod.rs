89db336a3e26b1da6d472eaf7c0052fd65196a35
Andy wu <lpageo@163.com>
2026-01-20 03:02:32 +0800

    typesetting: add basic pagination

--- src-tauri/src/typesetting/mod.rs
+++ src-tauri/src/typesetting/mod.rs
@@ -1,6 +1,7 @@
 pub mod font_manager;
 pub mod line_break;
 pub mod paragraph_layout;
+pub mod pagination;
 pub mod page_model;
 pub mod shaping;
 
@@ -9,5 +10,6 @@ pub use font_manager::{
 };
 pub use line_break::{break_glyph_run, BreakKind, LineBreak};
 pub use paragraph_layout::{layout_paragraph, ParagraphAlign, PositionedLine};
+pub use pagination::{paginate_flow, PageSlice};
 pub use page_model::{PageBox, PageMargins, PageSize, PageStyle};
 pub use shaping::{shape_mixed_text, shape_text, Glyph, GlyphRun, ShapingError};
